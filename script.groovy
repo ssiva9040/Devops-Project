cba6d1a726c6dfc2dc66e98d916315e1a44986eb
ssiva9040 <74784943+ssiva9040@users.noreply.github.com>
2024-01-03 22:38:04 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildImage() {
+     echo "building the image"
+                withCredientials([usernamePassword(credientialsId: 'Docker-hub-credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                    sh 'docker build -t sivanesansaravanan/demo-app:test .'
+                    sh "echo $PASS | docker login -u $USER --password-stdin"
+                    sh 'docker push sivanesansaravanan/demo-app:test'
+                }
+}
+def deployApplication() {
+    echo "deploying application"
+}
+
+return this

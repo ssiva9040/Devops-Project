f4b2e0166ac916ba2ecd06991c97334a47b29c0a
ssiva9040 <74784943+ssiva9040@users.noreply.github.com>
2024-01-04 21:58:00 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,9 +1,10 @@
 def buildImage() {
+     def buildNumber = env.BUILD_NUMBER
      echo "building the image"
                 withCredentials([usernamePassword(credentialsId: 'Docker-hub-credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-                    sh 'docker build -t sivanesansaravanan/demo-app:test .'
+                    sh 'docker build -t sivanesansaravanan/demo-app:test-${buildNumber} .'
                     sh "echo $PASS | docker login -u $USER --password-stdin"
-                    sh 'docker push sivanesansaravanan/demo-app:test'
+                    sh 'docker push sivanesansaravanan/demo-app:test-${buildNumber}'
                 }
 }
 def deployApplication() {
